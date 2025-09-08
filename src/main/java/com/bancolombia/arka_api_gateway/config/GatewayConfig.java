4a1bf764291f093a41b495c5b05b630de6ef969b
Daniel Lopez <daniellop199731@gmail.com>
2025-09-08 16:18:16 -0500

    Init Project

---
+++ src/main/java/com/bancolombia/arka_api_gateway/config/GatewayConfig.java
@@ -0,0 +1,39 @@
+package com.bancolombia.arka_api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+            // Hello World Service routes
+            .route("hello-world", r -> r
+                .path("/api/hellow/**")
+                .filters(f -> f.stripPrefix(2))
+                .uri("lb://hellow-world-service")
+            )
+            // Cotizador Service routes
+            /*.route("cotizador", r -> r
+                .path("/api/cotizador/**")
+                .filters(f -> f.stripPrefix(2))
+                .uri("lb://arka-cotizador")
+            )
+            // Gestor Solicitudes Service routes
+            .route("gestor-solicitudes", r -> r
+                .path("/api/gestor/**")
+                .filters(f -> f.stripPrefix(2))
+                .uri("lb://arca-gestor-solicitudes")
+            )*/
+            // Eureka Dashboard route
+            .route("eureka", r -> r
+                .path("/eureka/**")
+                .uri("http://localhost:8761")
+            )
+            .build();
+    }
+}
